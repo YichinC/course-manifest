9f1aaf745c85f6b6c50ffaa15a746c3e43c8636d
Ben Johnston <benj@princeton.edu>
2021-01-11 15:44:10 -0500

    fixes

--- collection.php
+++ collection.php
@@ -1,24 +1,23 @@
 <?php
 
-
 function renderList($file) {
 
 	 $returnObj = array();
 	 $returnObj["@context"] = "https://iiif.io/api/presentation/2/context.json";
-	 $returnObj["@id"] = "https://etcpanel.princeton.edu/LTI/mirador2/collection.json",
-	 $returnObj["@type"] = "sc:Collection",
-	 $returnObj["label"] = "Lecture Images",
-	 $returnObj["description"] = "A collection of lecture images",
+	 $returnObj["@id"] = "https://etcpanel.princeton.edu/LTI/mirador2/collection.json";
+	 $returnObj["@type"] = "sc:Collection";
+	 $returnObj["label"] = "Lecture Images";
+	 $returnObj["description"] = "A collection of lecture images";
 	 $returnObj["manifests"] = array();
-	 
+	
 	 $windowObjects = array();
 	 $manifests = array();
 	 $lines = file($file);
 
 	 foreach($lines as $key=>$line) {
 	  $parts = explode("	",$line);
-	  $man = new StdClass();
-	 die('OK');
+	  $man = array();
+
 	  $man["@type"] = "sc:Manifest";
 	  $man["@id"] = trim($parts[0]);
 	  //if(isset($parts[1])) { $man["label"] = trim($parts[1]); } else { $man["label"] = "";  }
@@ -38,14 +37,17 @@ function renderList($file) {
 	      $z->thumbnailNavigationPosition = 'far-bottom';
 	      $windowObjects[] = $z;
 	  }
+	  */
 	 }
-	 */
+
 	 
 	 //$returnObj['windowObjects'] = $windowObjects;
 	 //$returnObj['manifests'] = $manifests;
 
-	return json_encode($returnObj);
+	return json_encode($returnObj);	 
+
 }
+
 
 
 
